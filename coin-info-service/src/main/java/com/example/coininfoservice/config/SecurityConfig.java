022a3d8351193ccfa22827cf08e8f48ade2817d6
hieupham-thedev <hieu-pham.dev@outlook.com.vn>
2021-11-25 20:49:30 +0700

    Update userdetailservice

--- coin-info-service/src/main/java/com/example/coininfoservice/config/SecurityConfig.java
+++ coin-info-service/src/main/java/com/example/coininfoservice/config/SecurityConfig.java
@@ -1,27 +1,33 @@
 package com.example.coininfoservice.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@ComponentScan(basePackages = "com.example.coininfoservice.service")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    UserDetailsService userService;
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        return passwordEncoder;
+    }
+
+    @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        auth
-                .inMemoryAuthentication()
-                .withUser("user")
-                .password(encoder.encode("password"))
-                .roles("ADMIN");
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -29,6 +35,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .httpBasic()
+                .and()
+                .formLogin()
                 .and()
                 .authorizeRequests()
                 .anyRequest().authenticated();
